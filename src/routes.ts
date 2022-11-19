18c598aaf71141a9f81216593d7f0b8d25709ed2
adam-almeida <adamportifolio@gmail.com>
2022-11-19 12:08:00 -0300

    feat: create base routes

---
+++ src/routes.ts
@@ -0,0 +1,48 @@
+import { Router } from "express";
+
+export const router = Router();
+
+//list categories
+router.get("/categories", (req, res) => {
+    res.send("ok");
+});
+
+//create categories
+router.post("/categories", (req, res) => {
+    res.send("ok");
+});
+
+//list products
+router.get("/products", (req, res) => {
+    res.send("ok");
+});
+
+//create products
+router.post("/products", (req, res) => {
+    res.send("ok");
+});
+
+//get product by categorie
+router.get("/categories/:id/products", (req, res) => {
+    res.send("ok");
+});
+
+//list orders
+router.get("/orders", (req, res) => {
+    res.send("ok");
+});
+
+//create orders
+router.post("/orders", (req, res) => {
+    res.send("ok");
+});
+
+//change order status
+router.patch("/orders/:id", (req, res) => {
+    res.send("ok");
+});
+
+//delete order
+router.delete("/orders/:id", (req, res) => {
+    res.send("ok");
+});
