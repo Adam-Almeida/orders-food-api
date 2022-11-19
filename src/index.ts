c0397b6d8ec1e9679415c89af2b13b1c3d975527
adam-almeida <adamportifolio@gmail.com>
2022-11-19 11:48:31 -0300

    feat: create models and relations

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import express from "express";
 import mongoose from "mongoose";
 
-const port = 3001;
+const port = process.env.PORT || 3001;
 const connection = "mongodb://localhost:27017";
 
-const app = express();
+mongoose
+    .connect(connection)
+    .then(() => {
+        const app = express();
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-    mongoose
-        .connect(connection)
-        .then(() => console.log("Conectando ao mongoDb"))
-        .catch(() => console.log("Erro ao realizar conecção ao mongoDb"));
-});
+        console.log("Conectando ao mongoDb");
+    })
+    .catch(() => console.log("Erro ao se conectar com o mongoDb"));
