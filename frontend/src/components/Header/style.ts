3f7d3a11fb9624d599e3f3d663b418c39d19a7e0
adam-almeida <adamportifolio@gmail.com>
2022-11-20 15:05:15 -0300

    feat: styles from Header component

---
+++ frontend/src/components/Header/style.ts
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+    background-color: var(--theme);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 198px;
+    padding: 0 0.6rem;
+`;
+
+export const Content = styled.div`
+    width: 100%;
+    max-width: 1216px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    h1 {
+        color: var(--white);
+        font-size: 2rem;
+        font-weight: 600;
+    }
+
+    h2 {
+        color: var(--white);
+        font-size: 1rem;
+        font-weight: 400;
+    }
+`;
