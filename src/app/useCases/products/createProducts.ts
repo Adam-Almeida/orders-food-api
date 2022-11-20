915957f8c0d93860f159b7752369e844d2f672fa
adam-almeida <adamportifolio@gmail.com>
2022-11-19 21:57:12 -0300

    feat: createProduct usecase and upload image

--- src/app/useCases/products/createProducts.ts
+++ src/app/useCases/products/createProducts.ts
@@ -1,13 +1,28 @@
 import { Request, Response } from "express";
+import { Product } from "../../models/Product";
 
 class createProducts {
     async handle(req: Request, res: Response) {
         try {
+            const imagePath = req.file?.filename;
+            const { name, description, price, category, ingredients } =
+                req.body;
 
-            console.log(req.body)
+            if (!name || !description || !price || !category || !ingredients) {
+                res.status(400).json({ error: "Preencha todos os campos." });
+                return;
+            }
 
+            const product = await Product.create({
+                name,
+                description,
+                price: Number(price),
+                imagePath,
+                category,
+                ingredients: JSON.parse(ingredients),
+            });
 
-
+            res.status(201).json(product);
         } catch (error) {
             console.error(error);
             res.status(500).json({ error: "Internal server error." });
--- src/routes.ts
+++ src/routes.ts
@@ -10,9 +10,22 @@ import listProducts from "./app/useCases/products/listProducts";
 export const router = Router();
 
 const upload = multer({
+    fileFilter: function (req, file, callback) {
+        var ext = path.extname(file.originalname);
+        if(ext !== '.png' && ext !== '.jpg' && ext !== '.gif' && ext !== '.jpeg') {
+            return callback(new Error('Only images are allowed'))
+        }
+        callback(null, true)
+    },
+    limits:{
+        fileSize: 1024 * 1024
+    },
     storage: multer.diskStorage({
         destination(req, file, callback) {
             callback(null, path.resolve(__dirname, "..", "uploads"));
+        },
+        filename(req, file, callback) {
+            callback(null, `${Date.now()}-${file.originalname}`)
         },
     }),
 });
