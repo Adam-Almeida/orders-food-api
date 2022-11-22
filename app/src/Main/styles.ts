44415c3903f1aa22f98698f789a954eed5755c2a
adam-almeida <adamportifolio@gmail.com>
2022-11-22 19:22:45 -0300

    feat: create base containers

--- app/src/Main/styles.ts
+++ app/src/Main/styles.ts
@@ -5,4 +5,25 @@ const isAndroid = Platform.OS === "android";
 
 export const Container = styled.SafeAreaView`
     margin-top: ${isAndroid ? `${StatusBar.currentHeight}px` : "0"};
+    flex: 1;
+    background: #FAFAFA
 `;
+
+export const CategoriesContainer = styled.View`
+    height: 73px;
+    background: red;
+    margin-top: 34px;
+`;
+
+export const MenuContainer = styled.View`
+    height: 70px;
+    background-color: blue;
+    flex: 1;
+`;
+
+export const Footer = styled.View`
+    min-height: 110px;
+    background: #FFF;
+`;
+
+export const FooterContainer = styled.SafeAreaView``;
