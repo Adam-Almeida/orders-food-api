28410a6222329abb7ebf9a34b47f63f3351ef093
adam-almeida <adamportifolio@gmail.com>
2022-11-22 21:35:59 -0300

    feat: create opacity item flatList

--- app/src/components/Categories/index.tsx
+++ app/src/components/Categories/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { FlatList } from "react-native";
 
 import { categories } from "../../mocks/categories";
@@ -5,6 +6,12 @@ import { Text } from "../Text";
 import { Category, Icon } from "./styles";
 
 export function Categories() {
+    const [selectedCategory, setSelectedCategory] = useState("");
+
+    function handleSelectCategory(categoryId: string) {
+        setSelectedCategory(categoryId);
+    }
+
     return (
         <FlatList
             showsHorizontalScrollIndicator={false}
@@ -12,16 +19,28 @@ export function Categories() {
             contentContainerStyle={{ paddingRight: 24 }}
             data={categories}
             keyExtractor={(category) => category._id}
-            renderItem={({ item: category }) => (
-                <Category>
-                    <Icon>
-                        <Text>{category.icon}</Text>
-                    </Icon>
-                    <Text size={14} weight="600">
-                        {category.name}
-                    </Text>
-                </Category>
-            )}
+            renderItem={({ item: category }) => {
+                const isSelected = selectedCategory === category._id;
+
+                return (
+                    <Category
+                        onPress={() => handleSelectCategory(category._id)}
+                    >
+                        <Icon>
+                            <Text opacity={isSelected ? 1 : 0.5}>
+                                {category.icon}
+                            </Text>
+                        </Icon>
+                        <Text
+                            opacity={isSelected ? 1 : 0.5}
+                            size={14}
+                            weight="600"
+                        >
+                            {category.name}
+                        </Text>
+                    </Category>
+                );
+            }}
         />
     );
 }
