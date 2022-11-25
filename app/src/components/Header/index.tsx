242feb9ab582af663d5dec36ba5c3f4365f672c1
adam-almeida <adamportifolio@gmail.com>
2022-11-25 11:21:26 -0300

    feat: create selectedtable

--- app/src/components/Header/index.tsx
+++ app/src/components/Header/index.tsx
@@ -1,15 +1,23 @@
 import { Text } from "../Text";
 import { Container } from "./styles";
 
-export function Header() {
+interface HeaderProps {
+    selectedTable: string;
+}
+
+export function Header({ selectedTable }: HeaderProps) {
     return (
         <Container>
-            <Text size={14} opacity={0.8}>
-                Bem vindo(a) ao
-            </Text>
-            <Text size={24} weight="700">
-                FOOD<Text size={24}>APP</Text>
-            </Text>
+            {!selectedTable && (
+                <>
+                    <Text size={14} opacity={0.8}>
+                        Bem vindo(a) ao
+                    </Text>
+                    <Text size={24} weight="700">
+                        FOOD<Text size={24}>APP</Text>
+                    </Text>
+                </>
+            )}
         </Container>
     );
 }
