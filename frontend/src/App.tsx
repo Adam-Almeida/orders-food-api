26f2c95254046076d94e91bcb39d3767e481cdbf
adam-almeida <adamportifolio@gmail.com>
2022-11-28 16:08:01 -0300

    chore: add react router dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,5 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Global } from "./styles/Global";
