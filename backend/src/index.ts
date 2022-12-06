3b627ddf8c0716233ba490422f0cad6d6bacc7b9
adam-almeida <adamportifolio@gmail.com>
2022-12-05 21:12:48 -0300

    feat: deploy-backend-headers

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -17,9 +17,19 @@ mongoose
     .connect(connection!)
     .then(() => {
         app.use((req, res, next) => {
-            res.setHeader("Access-Control-Allow-Origin", `${urlFront}`);
-            res.setHeader("Access-Control-Allow-Methods", "*");
-            res.setHeader("Access-Control-Allow-Headers", "*");
+            res.setHeader("Access-Control-Allow-Origin", "*");
+            // res.setHeader("Access-Control-Allow-Methods", "*");
+            // res.setHeader("Access-Control-Allow-Headers", "*");
+
+            res.setHeader(
+                "Access-Control-Allow-Methods",
+                "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+            );
+            res.setHeader(
+                "Access-Control-Allow-Headers",
+                "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+            );
+
             next();
         });
 
